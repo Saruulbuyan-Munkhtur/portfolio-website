af960bd644b4e77519f7d37b0789c3f593b2e929
ErozionZeal <cedricmunkhtur2020@gmail.com>
2020-08-31 20:51:51 +0800

    some more styling

--- src/components/footer.js
+++ src/components/footer.js
@@ -8,8 +8,9 @@ import LinkedIn from '../assets/linkedin.svg'
 import Twitter from '../assets/twitter.svg'
 import Instagram from '../assets/instagram.svg'
 import Gmail from '../assets/gmail.svg'
+import Phone from '../assets/phone.svg'
 
-const Footer = () => (
+const Footer = ({ siteTitle }) => (
   <footer>
     <div className='social'>
     <Link><Github  className='icon'/></Link>
@@ -17,13 +18,23 @@ const Footer = () => (
     <Link ><LinkedIn className='icon'/></Link>
     <Link ><Instagram className='icon'/></Link>
     <Link ><Twitter className='icon'/></Link>
-    <Link ><Gmail className='icon'/></Link></div>
+    <Link ><Gmail className='icon'/></Link>
+    </div>
+    <div><Link ><Phone /> +976 99728346</Link></div>
     <div className='copyright'>
-    Saruulbuyan Munkhtur © {new Date().getFullYear()}, Built with {` `} <a href="https://www.gatsbyjs.org">Gatsby</a>
+    {siteTitle} © {new Date().getFullYear()}, Built with {` `} <a href="https://www.gatsbyjs.org">Gatsby</a>
     <div>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
     </div>
     
   </footer>
 )
+
+Footer.propTypes = {
+  siteTitle: PropTypes.string,
+}
+
+Footer.defaultProps = {
+  siteTitle: ``,
+}
 
 export default Footer
