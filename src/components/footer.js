7eb4a17803155e38f6f9e7bb8c0b42728ee24b7f
ErozionZeal <cedricmunkhtur2020@gmail.com>
2020-08-31 16:14:45 +0800

    Inserted social icons and edited them.

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,14 +1,28 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
-//import './footer.styles.scss'
+import '../CSS-Files/footer.css'
+import Github from '../assets/github.svg'
+import Medium from '../assets/medium.svg'
+import LinkedIn from '../assets/linkedin.svg'
+import Twitter from '../assets/twitter.svg'
+import Instagram from '../assets/instagram.svg'
+import Gmail from '../assets/gmail.svg'
 
 const Footer = () => (
   <footer>
-    This is a footer
-    © {new Date().getFullYear()}, Built with
-            {` `}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
+    <div className='social'>
+    <Link><Github  className='icon'/></Link>
+    <Link ><Medium className='icon'/></Link>
+    <Link ><LinkedIn className='icon'/></Link>
+    <Link ><Instagram className='icon'/></Link>
+    <Link ><Twitter className='icon'/></Link>
+    <Link ><Gmail className='icon'/></Link></div>
+    <div className='copyright'>
+    Saruulbuyan Munkhtur © {new Date().getFullYear()}, Built with {` `} <a href="https://www.gatsbyjs.org">Gatsby</a>
+    <div>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+    </div>
+    
   </footer>
 )
 
