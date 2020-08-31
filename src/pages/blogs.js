e541726f82c29aa358f7d056e5625994b95e8f81
ErozionZeal <cedricmunkhtur2020@gmail.com>
2020-08-31 15:17:18 +0800

    Made the contact and blog page

---
+++ src/pages/blogs.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Layout from '../components/layout'
+import SEO from '../components/seo'
+
+const BlogPage = () => {
+  return (
+    <Layout>
+      <SEO title="My Blogs" />
+      <h1>This is where my blogs will go</h1>
+      <p>This is the page where all my written blogs will be.</p>
+  </Layout>
+  )
+}
+
+export default BlogPage;
