daa50305ea06e27fa8cb211dc2b38367b55d76d1
Saruulbuyan Munkhtur <cedricmunkhtur2020@example.com>
2020-09-02 16:06:57 +0800

    Made an image component.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,10 @@
 import React from "react"
-import { Link } from "gatsby"
+//import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Personal2 from "../components/JSImages/personal2"
+import Image from "../components/JSImages/image"
 
 import '../CSS-Files/index.scss'
 
@@ -11,13 +12,14 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <div className="body">
-      <div style={{maxWidth:700, flex:1, marginRight: `10%`}}>
-        <h1 style={{borderBottom: `1px solid black`}}>Hi, I'm Saruulbuyan Munkhtur</h1>
+      <div className="Intro">
+        <h1>Hi, I'm Saruulbuyan Munkhtur</h1>
         <p>Welcome to my portfolio website</p>
       </div>
 
-      <div style={{maxWidth:500, flex: 1, borderRadius: `50px`,}}>
-        <Personal2 classname="Personal 2"/>
+      <div className="Pic">
+        {/* <Personal2 classname="Personal 2"/> */}
+        <Image filename="personal2.jpg" alt="Photo"/>
       </div>
     </div>
   </Layout>
