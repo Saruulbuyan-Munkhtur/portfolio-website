e0ec0708cf310020888db12c40baebf095198071
Saruulbuyan Munkhtur <cedricmunkhtur2020@example.com>
2020-09-14 10:00:07 +0800

    Creatied an Icons component

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,23 +5,15 @@ import React from "react"
 
 import '../scss/main.scss'
 
-import Github from '../assets/github.svg'
-import Medium from '../assets/medium.svg'
-import LinkedIn from '../assets/linkedin.svg'
-import Twitter from '../assets/twitter.svg'
-import Instagram from '../assets/instagram.svg'
-import Gmail from '../assets/gmail.svg'
+import Icons from './icons'
+
+
 import Phone from '../assets/phone.svg'
 
 const Footer = ({ siteTitle }) => (
   <footer>
-    <div className="social">
-    <Link to="https://github.com/ErozionZeal"><Github  className="icon2"/></Link>
-    <Link  to="https://medium.com/@SaruulbuyanMunkhtur"><Medium className="icon2"/></Link>
-    <Link  to="https://www.linkedin.com/in/saruulbuyan-munkhtur-a82257185/"><LinkedIn className="icon2"/></Link>
-    <Link  to="https://www.instagram.com/?hl=en"><Instagram className="icon2"/></Link>
-    <Link  to="https://www.Twitter.com"><Twitter className="icon2"/></Link>
-    <Link  to="@cedricmunkhtur2020@gmail.com"><Gmail className="icon2"/></Link>
+    <div>
+      <Icons width='50px' />
     </div>
     <div><Link ><Phone /> +976 99728346</Link></div>
     <div className="copyright">
---
+++ src/components/icons.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'gatsby';
+
+import Github from '../assets/github.svg'
+import Medium from '../assets/medium.svg'
+import LinkedIn from '../assets/linkedin.svg'
+import Twitter from '../assets/twitter.svg'
+import Instagram from '../assets/instagram.svg'
+import Gmail from '../assets/gmail.svg'
+
+export default function Icons({width}) {
+    return(
+        <div className="social">
+            <Link to="https://github.com/ErozionZeal"><Github  className="icon2" styles={{maxWidth: `${width}`}}/></Link>
+            <Link  to="https://medium.com/@SaruulbuyanMunkhtur"><Medium className="icon2" styles={{maxWidth: `${width}`}} styles={{maxWidth: `${width}`}}/></Link>
+            <Link  to="https://www.linkedin.com/in/saruulbuyan-munkhtur-a82257185/"><LinkedIn className="icon2" styles={{maxWidth: `${width}`}}/></Link>
+            <Link  to="https://www.instagram.com/?hl=en"><Instagram className="icon2" styles={{maxWidth: `${width}`}}/></Link>
+            <Link  to="https://www.Twitter.com"><Twitter className="icon2" styles={{maxWidth: `${width}`}}/></Link>
+            <Link  to="@cedricmunkhtur2020@gmail.com"><Gmail className="icon2" styles={{maxWidth: `${width}`}}/></Link>
+        </div>
+    )
+}
