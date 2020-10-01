027915b1d788afa3bc13ec2378a4c0c695f86c47
ErozionZeal <cedricmunkhtur2020@gmail.com>
2020-10-01 21:58:02 +0800

    Built and Deployed to Netlitfy

--- src/pages/blogs.js
+++ src/pages/blogs.js
@@ -14,7 +14,7 @@ const BlogPage = ({ location }) => {
       <Head />
       <SEO></SEO>
       <div className="container">
-        {location.state.tag? <BlogList tag={location.state.tag.topic}/>: <BlogList />}
+        {location.state? <BlogList tag={location.state.tag.topic}/>: <BlogList />}
       </div>
     </Layout>
   )
