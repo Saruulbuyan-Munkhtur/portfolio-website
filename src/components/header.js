5fd34f4962046de9acd70dfde0519b937f9bd3b1
Saruulbuyan Munkhtur <cedricmunkhtur2020@example.com>
2020-10-02 09:05:02 +0800

    fixed the blog page state error

--- src/components/header.js
+++ src/components/header.js
@@ -2,7 +2,7 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 import '../scss/main.scss'
-import Dark from '../assets/dark2.svg'
+//import Dark from '../assets/dark2.svg'
 
 const Header = ({ siteTitle }) => (
   <header className="navbar">
@@ -10,7 +10,7 @@ const Header = ({ siteTitle }) => (
       <Link to="/" className="Logo">{siteTitle}</Link>
       <ul>
         <li><Link to="/portfolio" className="Link">Portfolio</Link></li>
-        <li><Link to="/blogs" className="Link">My Blogs</Link></li>
+        <li><Link to="/blogs" className="Link" state={{tag: ""}}>My Blogs</Link></li>
       </ul>
     </div>
   </header>
--- src/pages/index.js
+++ src/pages/index.js
@@ -22,8 +22,11 @@ const IndexPage = () => (
     <section className="index">
       <div className="content">
         <h2>I'm Saruulbuyan Munkhtur. Welcome to my Blog and Portfolio.</h2>
+        <h4>
+          I am React TypeScript fullstack web developer. I also have experience in Data Science and Competitive Coding.
+        </h4>
         <p>
-          
+          I'm also an auto-didact and an aspiring polymath. I believe that information and knowledge are fully available to those who want to learn. My blogs are meant to help on my journey of learning and hopefully you can find some use in them as well.
         </p>
       </div>
     </section>
