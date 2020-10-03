ef2a34368287cd1fc628e794bca7e55d4b0503d0
ErozionZeal <cedricmunkhtur2020@gmail.com>
2020-10-03 12:10:24 +0800

    Added the topics buttons onto the blogs page

--- src/pages/blogs.js
+++ src/pages/blogs.js
@@ -2,18 +2,39 @@ import React from 'react';
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 import Head from "../components/head"
+import { useStaticQuery, graphql } from "gatsby"
 import BlogList from '../components/blogList'
+import TagList from '../components/tagList'
 
 import '../scss/main.scss'
 
 
 const BlogPage = ({ location }) => {
+  const data = useStaticQuery(graphql`{
+    allMarkdownRemark {
+      edges {
+        node {
+          frontmatter {
+            topics
+          }
+        }
+      }
+    }
+  }`)
+
+  let alltopics = [];
+  data.allMarkdownRemark.edges.map((edge) => {
+    alltopics = alltopics.concat(edge.node.frontmatter.topics)
+  })
   
   return (
     <Layout>
       <Head />
       <SEO></SEO>
       <div className="container">
+        <div className="blogs-tagList"> 
+          <TagList topics={alltopics}/>
+        </div>
         {location.state? <BlogList tag={location.state.tag.topic}/>: <BlogList />}
       </div>
     </Layout>
@@ -21,3 +42,4 @@ const BlogPage = ({ location }) => {
 }
 
 export default BlogPage;
+
