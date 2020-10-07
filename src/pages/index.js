04ff50b25e3a6fb40122edd8cc2666ea1e53c6e7
Saruulbuyan Munkhtur <cedricmunkhtur2020@example.com>
2020-10-07 20:29:45 +0800

    😀😀 Added an illustration to the homePage

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import React from "react"
 import Layout from "../components/layout"
 import Head from "../components/head"
 import SEO from "../components/seo"
+import SVG from "../assets/code_thinking.svg"
 
 //import backgroundImage from "../components"
 //import Image from "../components/JSImages/image"
@@ -21,8 +22,10 @@ const IndexPage = () => (
     <Head />
     <SEO title="Home" />
     <section className="homePage-index">
+      <div className="homePage-illustration">
+        <SVG className="homePage-SVG"/>
+      </div>
       <div className="homePage-content">
-        <div>
           <h1>I'm Saruulbuyan Munkhtur. Welcome to my Blog and Portfolio.</h1>
           <h2>
             I am React TypeScript fullstack web developer. I also have experience in Data Science and Competitive Coding.
@@ -30,7 +33,6 @@ const IndexPage = () => (
           <p>
             I'm also an auto-didact and an aspiring polymath. I believe that information and knowledge are fully available to those who want to learn. My blogs are meant to help on my journey of learning and hopefully you can find some use in them as well.
           </p>
-        </div>
       </div>
     </section>
   </Layout>
