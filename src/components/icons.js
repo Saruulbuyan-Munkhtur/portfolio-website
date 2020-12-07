51c4f9fbcd091702b61e0338c0f0a6aa97c41a5e
Saruulbuyan Munkhtur <cedricmunkhtur2020@example.com>
2020-12-07 22:08:24 +0800

    github profille link fix

--- src/components/icons.js
+++ src/components/icons.js
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import Github from '../assets/github.svg'
 import Medium from '../assets/medium.svg'
 import LinkedIn from '../assets/linkedin.svg'
@@ -12,7 +11,7 @@ import '../scss/main.scss'
 export default function Icons(width) {
     return (
         <div className="icons">
-            <a href="https://github.com/ErozionZeal" target="_blank"><Github  className="icon2" styles={{maxWidth: `${width}`}}/></a>
+            <a href="https://github.com/Saruulbuyan-Munkhtur" target="_blank"><Github  className="icon2" styles={{maxWidth: `${width}`}}/></a>
             <a  href="https://medium.com/@SaruulbuyanMunkhtur" target="_blank"><Medium className="icon2" styles={{maxWidth: `${width}`}}/></a>
             <a  href="https://www.linkedin.com/in/saruulbuyan-munkhtur-a82257185/" target="_blank"><LinkedIn className="icon2" styles={{maxWidth: `${width}`}}/></a>
             <a  href="https://www.instagram.com/saruulbuyanmunkhtur/?hl=en" target="_blank"><Instagram className="icon2" styles={{maxWidth: `${width}`}}/></a>
