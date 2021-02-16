e4f6697fedb181e1b2c815083c267fff35856de8
Saruulbuyan-Munkhtur <Cedricmunkhtur2020@gmail.com>
2021-02-16 18:49:06 +0800

    Added Seo in Layout globally

--- src/layout/layout.js
+++ src/layout/layout.js
@@ -1,45 +1,38 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import React from "react"
 import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
-// import PageTransition from 'gatsby-plugin-page-transitions';
-// import { TransitionProvider, TransitionViews } from "gatsby-plugin-transitions";
+import { StaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
 import Footer from "./footer"
+import Seo from "./seo"
 // import Sidebar from "./sidebar"
 import '../scss/main.scss'
 
 
-const Layout = ({ children, location }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-          description
-          author
-        }
-      }
-    }
-  `)
 
-  return (
-        <div  className="body"> 
-            <Header siteTitle={data.site.siteMetadata.title}/>
-            <main>
-              {children}
-            </main>
-            <Footer siteTitle={data.site.siteMetadata.title}/>
-        </div>
-  )
-}
+const Layout = ({ children, seo }) => (
+  <StaticQuery 
+    query={graphql`
+      query {
+        strapiGlobal {
+          defaultSeo {
+            metaTitle
+            metaDescription
+          }
+        }
+      } 
+    `}
+    render={(data) => (
+      <div className="body"> 
+          <Seo seo={seo} />
+          <Header siteTitle={data.strapiGlobal.defaultSeo.metaTitle}/>
+            <main>{children}</main>
+          <Footer siteTitle={data.strapiGlobal.defaultSeo.metaTitle}/>
+      </div>
+    )}
+  />
+
+)
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
