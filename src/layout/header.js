5114aa58644aeb5e339bb8fdfa9b0461d7264a5f
Saruulbuyan-Munkhtur <Cedricmunkhtur2020@gmail.com>
2021-02-17 17:59:49 +0800

    Header logo resize update

--- src/layout/header.js
+++ src/layout/header.js
@@ -7,10 +7,12 @@ import Hexagons from '../assets/geometric_hexagon.svg'
 
 const Header = ({ siteTitle }) => (
   <header className="navbar-main">
-      <Link to="/" className="navbar-logo"><Hexagons /></Link>
+      <div>
+        <Link to="/" className="navbar-logo"><Hexagons /></Link>
+      </div>
       <ul className="navbar-list">
-        <li><Link to="/portfolio" className="navbar-link">Portfolio</Link></li>
-        <li><Link to="/blogs" className="navbar-link" state={{tag: ""}}>Blogs</Link></li>
+        <li><Link to="/portfolio" className="navbar-link" aria-label="See more in my Portfolio">Portfolio</Link></li>
+        <li><Link to="/blogs" className="navbar-link" aria-label="Read some of my blogs" state={{tag: ""}}>Blogs</Link></li>
       </ul>
   </header>
 )
