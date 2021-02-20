82d3ce2539aa07fede90e45dae86087e5e3675ac
Saruulbuyan-Munkhtur <Cedricmunkhtur2020@gmail.com>
2021-02-20 17:48:09 +0800

    fixed the header logo png

--- src/layout/header.js
+++ src/layout/header.js
@@ -1,14 +1,14 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import Img from "gatsby-image";
 import '../scss/main.scss'
-import Hexagons from '../assets/geometric_hexagon.svg'
 
 
-const Header = ({ siteTitle }) => (
+const Header = ({ globalData }) => (
   <header className="navbar-main">
       <div>
-        <Link to="/" className="navbar-logo"><Hexagons /></Link>
+        <Link to="/" className="navbar-logo"><Img fixed={globalData.favicon.childImageSharp.fixed}/></Link>
       </div>
       <ul className="navbar-list">
         <li><Link to="/portfolio" className="navbar-link" aria-label="See more in my Portfolio">Portfolio</Link></li>
--- src/layout/layout.js
+++ src/layout/layout.js
@@ -1,7 +1,6 @@
-import React from "react"
-import PropTypes from "prop-types"
-import { StaticQuery, graphql } from "gatsby"
-import Img from 'gatsby-image';
+import React from "react";
+import PropTypes from "prop-types";
+import { StaticQuery, graphql } from "gatsby";
 
 import Header from "./header"
 import Footer from "./footer"
@@ -22,7 +21,14 @@ const Layout = ({ children, seo }) => (
           }
           profileImage {
             childImageSharp {
-              fixed(width:55) {
+              fixed(height: 55) {
+                ...GatsbyImageSharpFixed
+              }
+            }
+          }
+          favicon {
+            childImageSharp {
+              fixed(width: 60) {
                 ...GatsbyImageSharpFixed
               }
             }
@@ -33,8 +39,7 @@ const Layout = ({ children, seo }) => (
     render={(data) => (
       <div className="body"> 
           <Seo seo={seo} />
-          <Header siteTitle={data.strapiGlobal.defaultSeo.metaTitle}/>
-          <Img fixed={data.strapiGlobal.profileImage.fixed}/>
+          <Header globalData={data.strapiGlobal}/>
             <main>
               <Sidebar profileImage={data.strapiGlobal.profileImage.childImageSharp.fixed} />
               {children}
